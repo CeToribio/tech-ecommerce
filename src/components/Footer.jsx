bbd405491886153572a0420704dc39664b820aa7
CeToribio <anctoribio@gmail.com>
2023-05-16 13:55:47 -0500

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer(){
+    return(
+        <>
+        <p><strong>Contáctanos</strong></p>
+        <p>info@paulette.com</p>
+        <p>385 Noah Place Suite 878</p>
+        <p>877-255-7945</p>
+        <div>
+        <i className="fi fi-brands-facebook"></i>
+        <i className="fi fi-brands-instagram"></i>
+        <i className="fi fi-brands-twitter"></i>
+        </div>
+        </>
+    )
+}
+
+export default Footer
